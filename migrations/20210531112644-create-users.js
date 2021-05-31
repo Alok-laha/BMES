82239161ab6027928e86bfb797bfd4d7ca601bcb
Alok-laha <aloklaha98@gmail.com>
2021-05-31 17:36:05 +0530

    updated at removed

--- migrations/20210531112644-create-users.js
+++ migrations/20210531112644-create-users.js
@@ -24,11 +24,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     });
   },
